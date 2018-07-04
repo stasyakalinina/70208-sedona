1c5644c5ff81310cd24cbef3cd6480c35f3115ba
Anastasia Kalinina <stasya.kalinina@gmail.com>
2018-07-04 18:51:37 +0500

    Добавлена валидация полей и анимация ошибки

--- js/form-script.js
+++ js/form-script.js
@@ -1,7 +1,26 @@
 var button = document.querySelector(".btn-search-hotels");
 var form = document.querySelector(".search-form");
+var checkIn = form.querySelector("[name=check-in]");
+var checkOut = form.querySelector("[name=check-out]");
 
 button.addEventListener("click", function (evt) {
     evt.preventDefault();
     form.classList.toggle("search-form-show");
+    checkIn.focus();
+});
+
+form.addEventListener("submit", function (evt) {
+    if (!checkIn.value || !checkOut) {
+        evt.preventDefault();
+        form.classList.add("search-form-error");
+    }
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        if (form.classList.contains("search-form-show")) {
+            evt.preventDefault();
+            form.classList.remove("search-form-show");
+        }   
+    }
 });
